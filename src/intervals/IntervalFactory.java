3d7d663131200b5de6659c466eb3c201f865ba8b
franciscomaestre <francisco.maestretorreblanca@alum.uca.es>
2015-04-08 23:43:17 +0200

    R. Replace Type Code With Subclasses - Factoria Modificada

--- src/intervals/IntervalFactory.java
+++ src/intervals/IntervalFactory.java
@@ -5,8 +5,18 @@ public class IntervalFactory {
 
 	public static Interval getInterval(double minimum, double maximum, Opening opening) {
 	    
-		return new Interval(minimum, maximum, opening);
+    	switch (opening) {
+            case BOTH_OPENED:
+                return new IntervalBothOpened(minimum,maximum);
+            case LEFT_OPENED:
+                return new IntervalLeftOpened(minimum,maximum);
+            case RIGHT_OPENED:
+                return new IntervalRightOpened(minimum,maximum);
+            case UNOPENED:
+                return new IntervalUnopened(minimum,maximum);
+            default:
+                throw new IllegalArgumentException("Incorrect type code value");
+        }
 		
 	}
 }
-
