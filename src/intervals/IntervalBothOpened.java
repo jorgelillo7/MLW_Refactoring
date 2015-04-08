44d18131794685c35338436396803863f946fcff
franciscomaestre <francisco.maestretorreblanca@alum.uca.es>
2015-04-09 00:26:46 +0200

    R. Replace Conditional with Polymorphism - IntervalBothOpened include(interval) Completo

--- src/intervals/IntervalBothOpened.java
+++ src/intervals/IntervalBothOpened.java
@@ -18,6 +18,22 @@ public class IntervalBothOpened extends Interval{
     public boolean includes(Interval interval) {
         boolean minimumIncluded = this.includes(interval.getMinimum());
         boolean maximumIncluded = this.includes(interval.getMaximum());
+        switch (interval.getOpening()) {
+        case BOTH_OPENED:
+            return (minimumIncluded || getMinimum() == interval.getMinimum())
+                    && (maximumIncluded || getMaximum() == interval.getMaximum());
+        case LEFT_OPENED:
+            return (minimumIncluded || getMinimum() == interval.getMinimum())
+                    && (maximumIncluded);
+        case RIGHT_OPENED:
+            return (minimumIncluded)
+                    && (maximumIncluded || getMaximum() == interval.getMaximum());
+        case UNOPENED:
+            return (minimumIncluded) && (maximumIncluded);
+        default:
+            assert false;
+            return false;
+        }
     }
 
 }
