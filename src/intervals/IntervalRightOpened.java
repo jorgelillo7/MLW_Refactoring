fb832fcf185d69a425f51f5444ddce9070097419
franciscomaestre <francisco.maestretorreblanca@alum.uca.es>
2015-04-08 23:36:41 +0200

    R. Replace Type Code With Subclasses - IntervalRightOpened Fix Constructor

--- src/intervals/IntervalRightOpened.java
+++ src/intervals/IntervalRightOpened.java
@@ -2,11 +2,13 @@ package intervals;
 
 public class IntervalRightOpened extends Interval {
 
-    public IntervalRightOpened(double minimum, double maximum, Opening opening) {
-        super(minimum, maximum, opening);
+
+    
+    public IntervalRightOpened(double minimum, double maximum) {
+        super(minimum, maximum);
         // TODO Auto-generated constructor stub
     }
-    
+
     Opening getOpening() {
         return Opening.RIGHT_OPENED;
     }
