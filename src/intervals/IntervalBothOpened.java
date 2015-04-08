39f2defeda45e78416d7c016616178b1c7231e3d
franciscomaestre <francisco.maestretorreblanca@alum.uca.es>
2015-04-08 23:33:18 +0200

    R. Replace Type Code With Subclasses - IntervalBothOpened getOpening

--- src/intervals/IntervalBothOpened.java
+++ src/intervals/IntervalBothOpened.java
@@ -6,5 +6,9 @@ public class IntervalBothOpened extends Interval{
         super(minimum, maximum, opening);
         // TODO Auto-generated constructor stub
     }
+    
+    Opening getOpening() {
+        return Opening.BOTH_OPENED;
+    }
 
 }
