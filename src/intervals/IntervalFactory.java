2a515f5a102f73becdc797551548bc02092f992c
franciscomaestre <francisco.maestretorreblanca@alum.uca.es>
2015-04-08 23:26:43 +0200

    R. Replace Type Code With Subclasses - Mofificamos la Factoria

--- src/intervals/IntervalFactory.java
+++ src/intervals/IntervalFactory.java
@@ -4,6 +4,7 @@ package intervals;
 public class IntervalFactory {
 
 	public static Interval getInterval(double minimum, double maximum, Opening opening) {
+	    
 		return new Interval(minimum, maximum, opening);
 	}
 }
