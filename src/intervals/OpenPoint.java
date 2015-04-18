cff66b39b63b19b3ee927899e5c64158959d81f7
jorgelillo7 <jorge.lillo9@gmail.com>
2015-04-18 14:34:00 +0200

    Hacemos que eclipse autocomplete ClosePoint y OpenPoint

---
+++ src/intervals/OpenPoint.java
@@ -0,0 +1,28 @@
+package intervals;
+
+public class OpenPoint extends Point {
+
+    public OpenPoint(double value) {
+        super(value);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public boolean menor(Point point) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean menor(OpenPoint point) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean menor(ClosePoint point) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
