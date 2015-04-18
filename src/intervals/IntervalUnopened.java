014aac29f0ce89a7961f02ef76cb661a858244bd
jorgelillo7 <jorge.lillo9@gmail.com>
2015-04-18 11:19:56 +0200

    Copiamos los métodos en IntervalUnopened

--- src/intervals/IntervalUnopened.java
+++ src/intervals/IntervalUnopened.java
@@ -36,5 +36,22 @@ public class IntervalUnopened extends Interval{
             return false;
         }
     }
+    
+    public boolean includes(IntervalBothOpened interval){
+        return false;
+        
+    }
+    
+    public boolean includes(IntervalLeftOpened interval) {
+        return false;
+    }
+    
+    public boolean includes(IntervalRightOpened interval) {
+        return false;
+    }
+    
+    public boolean includes(IntervalUnopened interval) {
+        return false;
+    }
 
 }
